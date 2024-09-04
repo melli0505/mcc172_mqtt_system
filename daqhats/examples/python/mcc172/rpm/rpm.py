cf0daeb6fc178ec46d67b27cd03eb20d713084c6
melli0505 <dmelli0505@gmail.com>
2024-09-04 14:48:32 +0900

    rpm tachometer

---
+++ daqhats/examples/python/mcc172/rpm/rpm.py
@@ -0,0 +1,41 @@
+
+import time
+from paho.mqtt import client as mqtt
+from threading import Lock
+import RPi.GPIO as GPIO
+
+sensor = 22
+
+GPIO.setmode(GPIO.BCM) 
+GPIO.setup(sensor,GPIO.IN) 
+
+sample = 1000
+count = 0
+
+start = 0
+end = 0
+
+broker_address =  "SERVER_ADDRESS"
+client = mqtt.Client("motor_rpm")
+client.connect(broker_address, 1883)
+
+def get_rpm(c):
+	global count, rpm
+	count = count + 1
+
+print("Add event listener...")
+GPIO.add_event_detect(sensor, GPIO.RISING, callback=get_rpm) 
+print("Start!")
+
+try:
+	start = time.time()
+	while True: 
+		end = time.time()
+		if end - start >= 60:
+			count = int(count / 2)
+			client.publish("motor_rpm", str(count))
+			count = 0
+			start = end
+except KeyboardInterrupt:
+	print( "  Quit")
+	GPIO.cleanup()
